bd2c269a8e34104f88100f1c4f103523e3bd6941
g-i-t-h-u-b-u-s-e-r-n-a-m-e <m@crowcastle.net>
2026-04-29 20:51:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function link(){
+    if (1 == 2){
+        
+    } else {
+        alert("Incorrect username/password, please try again")
+    }
+}
+function enlist(){
+    alert("You joined our mailing list, thanks for signing up!")
+}
